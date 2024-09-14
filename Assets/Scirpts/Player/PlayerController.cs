e02a28884ef10652243b40ee63d71525ce1211c8
DuganChandler <duganchandler10@gmail.com>
2024-09-14 19:46:31 -0400

    Changed player movement system

--- Assets/Scirpts/Player/PlayerController.cs
+++ Assets/Scirpts/Player/PlayerController.cs
@@ -1,51 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Callbacks;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour {
-    private Rigidbody2D rigidbody2D;
-    private CircleCollider2D circleCollider2D;
-    [SerializeField] private LayerMask groundLayer;
-    [Range(0, 10f)] [SerializeField] private float speed = 0f;
+    [SerializeField] LayerMask groundLayer;
+    public float walkSpeed = 5f;
+    public float jumpImpulse = 4f;
+    Vector2 moveInput;
+    public bool IsMoving { get; private set; }
+    Rigidbody2D rigidbody2D;
+    CircleCollider2D circleCollider2D;
 
-    float horizontal = 0f;
-    private bool isFacingRight = true;
-    bool jump = false;
+    void Awake() {
+        rigidbody2D = GetComponent<Rigidbody2D>();    
+        circleCollider2D = GetComponent<CircleCollider2D>();
+    }
 
     void Start() {
-       rigidbody2D = GetComponent<Rigidbody2D>(); 
-       circleCollider2D = GetComponent<CircleCollider2D>(); 
+
     }
 
     void Update() {
-        horizontal = Input.GetAxisRaw("Horizontal") * speed;
-
-        if (isOnGround() && horizontal.Equals(0)) {
-            // idle
-        } else if (isOnGround() && (horizontal > 0 || horizontal < 0)) {
-            // moving
-        }
-
-        if (isOnGround() && Input.GetButtonDown("Jump")){
-            jump = true;
-        } 
     }
 
     void FixedUpdate() {
-       float moveFactor = horizontal * Time.fixedDeltaTime; 
-
-       // Movement
-       rigidbody2D.velocity = new Vector2(moveFactor * 10f, rigidbody2D.velocity.y);
-
-       if (jump) {
-            float jumpvel = 2f;
-            rigidbody2D.velocity = Vector2.up * jumpvel;
-            jump = false;
-       }
+        rigidbody2D.velocity = new Vector2 (moveInput.x * walkSpeed, rigidbody2D.velocity.y);
     }
 
     private bool isOnGround() {
        RaycastHit2D hit = Physics2D.CircleCast(circleCollider2D.bounds.center, circleCollider2D.radius, Vector2.down, 0.1f, groundLayer);
        return hit.collider != null;
+    }
+    
+    public void onMove(InputAction.CallbackContext context) {
+        moveInput = context.ReadValue<Vector2>();
+        IsMoving = moveInput != Vector2.zero;
+    }
+
+    public void onJump(InputAction.CallbackContext context) {
+        if (context.started && isOnGround()) {
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpImpulse);
+        }
     }
 }
